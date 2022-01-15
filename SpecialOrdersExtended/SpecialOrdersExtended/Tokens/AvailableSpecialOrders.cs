1d7573c6092ecd4f6bb97550f7ab79a365d51800
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-01-15 13:57:20 -0500

    Fix available orders appearing availble before quest board is unlocked.

--- SpecialOrdersExtended/SpecialOrdersExtended/Tokens/AvailableSpecialOrders.cs
+++ SpecialOrdersExtended/SpecialOrdersExtended/Tokens/AvailableSpecialOrders.cs
@@ -8,8 +8,15 @@ internal class AvailableSpecialOrders : AbstractToken
     /// <inheritdoc/>
     public override bool UpdateContext()
     {
-        List<string>? specialOrderNames = Game1.player?.team?.availableSpecialOrders?.Select((SpecialOrder s) => s.questKey.ToString()).OrderBy(a => a).ToList()
-            ?? SaveGame.loaded?.availableSpecialOrders?.Select((SpecialOrder s) => s.questKey.ToString()).OrderBy(a => a).ToList();
-        return this.UpdateCache(specialOrderNames);
+        if (SpecialOrder.IsSpecialOrdersBoardUnlocked())
+        {
+            List<string>? specialOrderNames = Game1.player?.team?.availableSpecialOrders?.Select((SpecialOrder s) => s.questKey.ToString()).OrderBy(a => a).ToList()
+                ?? SaveGame.loaded?.availableSpecialOrders?.Select((SpecialOrder s) => s.questKey.ToString()).OrderBy(a => a).ToList();
+            return this.UpdateCache(specialOrderNames);
+        }
+        else
+        {
+            return this.UpdateCache(new List<string>());
+        }
     }
 }

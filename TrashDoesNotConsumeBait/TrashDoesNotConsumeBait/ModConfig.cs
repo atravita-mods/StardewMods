bb68e154a97faee905ccede9937961266b713cfd
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-03-24 17:22:21 -0400

    Initial commit

---
+++ TrashDoesNotConsumeBait/TrashDoesNotConsumeBait/ModConfig.cs
@@ -0,0 +1,43 @@
+﻿namespace TrashDoesNotConsumeBait;
+
+/// <summary>
+/// The configuration class for this mod.
+/// </summary>
+public class ModConfig
+{
+    private float consumeChanceNormal = 1f;
+    private float consumeChancePreserving = 0.5f;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether or not tackles/bait should be automatically refilled.
+    /// </summary>
+    public bool AutomaticRefill { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether tackles should be only replaced with the same type of tackle.
+    /// </summary>
+    public bool SameTackleOnly { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether bait should only be replaced with the same type of bait.
+    /// </summary>
+    public bool SameBaitOnly { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets chance of consuming bait/tackle normally.
+    /// </summary>
+    public float ConsumeChanceNormal
+    {
+        get => this.consumeChanceNormal;
+        set => this.consumeChanceNormal = Math.Clamp(value, 0f, 1f);
+    }
+
+    /// <summary>
+    /// Gets or sets chance of consuming bait/tackle with the Preserving enchantment.
+    /// </summary>
+    public float ConsumeChancePreserving
+    {
+        get => this.consumeChancePreserving;
+        set => this.consumeChancePreserving = Math.Clamp(value, 0f, 1f);
+    }
+}
---
+++ TrashDoesNotConsumeBait/TrashDoesNotConsumeBait/ModEntry.cs
@@ -0,0 +1,91 @@
+﻿using System.Reflection;
+using AtraShared.Integrations;
+using AtraShared.Utils.Extensions;
+using HarmonyLib;
+using StardewModdingAPI.Events;
+
+namespace TrashDoesNotConsumeBait;
+
+/// <inheritdoc/>
+internal class ModEntry : Mod
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    /// <summary>
+    /// Gets the logger for this mod.
+    /// </summary>
+    internal static IMonitor ModMonitor { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the config instance for this mod.
+    /// </summary>
+    internal static ModConfig Config { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    /// <inheritdoc/>
+    public override void Entry(IModHelper helper)
+    {
+        ModMonitor = this.Monitor;
+        I18n.Init(helper.Translation);
+
+        try
+        {
+            Config = this.Helper.ReadConfig<ModConfig>();
+        }
+        catch
+        {
+            this.Monitor.Log(I18n.IllFormatedConfig(), LogLevel.Warn);
+            Config = new();
+        }
+
+        helper.Events.GameLoop.GameLaunched += this.SetUpConfig;
+        this.ApplyPatches(new Harmony(this.ModManifest.UniqueID));
+    }
+
+    private void SetUpConfig(object? sender, GameLaunchedEventArgs e)
+    {
+        GMCMHelper helper = new(this.Monitor, this.Helper.Translation, this.Helper.ModRegistry, this.ModManifest);
+        if(!helper.TryGetAPI())
+        {
+            return;
+        }
+        helper.Register(
+            reset: () => Config = new(),
+            save: () => this.Helper.WriteConfig(Config));
+        foreach (PropertyInfo property in typeof(ModConfig).GetProperties())
+        {
+
+            if (property.PropertyType == typeof(bool))
+            {
+                helper.AddBoolOption(
+                    property: property,
+                    getConfig: () => Config);
+            }
+        }
+        helper.AddPageHere("CheatyStuff", I18n.CheatyStuffTitle);
+        foreach (PropertyInfo property in typeof(ModConfig).GetProperties())
+        {
+            if (property.PropertyType == typeof(float))
+            {
+                helper.AddFloatOption(
+                    property: property,
+                    getConfig: () => Config,
+                    min: 0f,
+                    max: 1f,
+                    interval: 0.01f);
+            }
+        }
+    }
+
+    private void ApplyPatches(Harmony harmony)
+    {
+        try
+        {
+            harmony.PatchAll();
+        }
+        catch (Exception ex)
+        {
+            ModMonitor.Log($"Mod crashed while applying harmony patches:\n\n{ex}", LogLevel.Error);
+        }
+        harmony.Snitch(this.Monitor, this.ModManifest.UniqueID);
+    }
+}

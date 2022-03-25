4dee65a849240f7ab0001e046276d5e96344f0c6
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-03-24 23:52:43 -0400

    fixes

--- TrashDoesNotConsumeBait/TrashDoesNotConsumeBait/HarmonyPatches/ToolbarPatches.cs
+++ TrashDoesNotConsumeBait/TrashDoesNotConsumeBait/HarmonyPatches/ToolbarPatches.cs
@@ -15,37 +15,44 @@ internal static class ToolbarPatches
     [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "Harmony convention")]
     private static void PostfixRightClick(List<ClickableComponent> ___buttons, int x, int y)
     {
-        if (Game1.player.UsingTool || Game1.IsChatting
-            || (Game1.player.ActiveObject?.Category is not SObject.baitCategory && Game1.player.ActiveObject?.Category is not SObject.tackleCategory))
+        try
         {
-            return;
-        }
-        foreach (ClickableComponent button in ___buttons)
-        {
-            if (button.containsPoint(x, y) && int.TryParse(button.name, out int val) && Game1.player.Items[val] is FishingRod fishingRod)
+            if (Game1.player.UsingTool || Game1.IsChatting
+                || (Game1.player.ActiveObject?.Category is not SObject.baitCategory && Game1.player.ActiveObject?.Category is not SObject.tackleCategory))
             {
-                SObject activeObj = Game1.player.ActiveObject;
-                Game1.player.ActiveObject = null;
-                switch (activeObj.Category)
+                return;
+            }
+            foreach (ClickableComponent button in ___buttons)
+            {
+                if (button.containsPoint(x, y) && int.TryParse(button.name, out int val) && Game1.player.Items[val] is FishingRod fishingRod)
                 {
-                    case SObject.baitCategory:
-                        if (fishingRod.attachments[0] is SObject bait)
-                        {
-                            Game1.player.ActiveObject = bait;
-                        }
-                        fishingRod.attachments[0] = activeObj;
-                        return;
-                    case SObject.tackleCategory:
-                        if (fishingRod.attachments[1] is SObject tackle)
-                        {
-                            Game1.player.ActiveObject = tackle;
-                        }
-                        fishingRod.attachments[1] = activeObj;
-                        return;
-                    default:
-                        return;
+                    SObject activeObj = Game1.player.ActiveObject;
+                    Game1.player.ActiveObject = null;
+                    switch (activeObj.Category)
+                    {
+                        case SObject.baitCategory:
+                            if (fishingRod.attachments[0] is SObject bait)
+                            {
+                                Game1.player.ActiveObject = bait;
+                            }
+                            fishingRod.attachments[0] = activeObj;
+                            return;
+                        case SObject.tackleCategory:
+                            if (fishingRod.attachments[1] is SObject tackle)
+                            {
+                                Game1.player.ActiveObject = tackle;
+                            }
+                            fishingRod.attachments[1] = activeObj;
+                            return;
+                        default:
+                            return;
+                    }
                 }
             }
+        }
+        catch (Exception ex)
+        {
+            ModEntry.ModMonitor.Log($"Patch failed while trying to swap out bait or tackle...\n\n{ex}", LogLevel.Error);
         }
     }
 }

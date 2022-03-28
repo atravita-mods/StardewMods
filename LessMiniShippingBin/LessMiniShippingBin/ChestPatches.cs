521d397bba96fb38faeb2c51466601a06a85494a
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-03-28 11:09:29 -0400

    Cleanup

--- LessMiniShippingBin/LessMiniShippingBin/ChestPatches.cs
+++ LessMiniShippingBin/LessMiniShippingBin/ChestPatches.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using HarmonyLib;
+﻿using HarmonyLib;
 using StardewValley.Objects;
 
 namespace LessMiniShippingBin;
--- LessMiniShippingBin/LessMiniShippingBin/docs/changelog.md
+++ LessMiniShippingBin/LessMiniShippingBin/docs/changelog.md
@@ -1,6 +1,10 @@
 ﻿Changelog
 ===============
 
+### Version 0.2.0
+
+Also expands the junimo chest.
+
 ### Version 0.1.0
 
 Initial upload

127ebb6bf220374d0c935a59247c0bcfb2e4e8db
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-04-02 16:20:53 -0400

    fix docs

--- SpecialOrdersExtended/SpecialOrdersExtended/Tokens/AbstractToken.cs
+++ SpecialOrdersExtended/SpecialOrdersExtended/Tokens/AbstractToken.cs
@@ -15,14 +15,14 @@ internal abstract class AbstractToken
     /// <summary>
     /// Whether or not the token allows input. Default, true.
     /// </summary>
-    /// <returns>true - all derived tokens should allow input.</returns>
+    /// <returns>false - we don't need input.</returns>
     public virtual bool AllowsInput() => false;
 
     /// <summary>
     /// Whether or not the token will produce multiple outputs, depending on the input to the token.
     /// </summary>
     /// <param name="input">Input to token.</param>
-    /// <returns>False (no need for my own inputs).</returns>
+    /// <returns>True - every token can have multiple outputs.</returns>
     public virtual bool CanHaveMultipleValues(string? input = null) => true;
 
     /// <summary>Get whether the token is available for use.</summary>

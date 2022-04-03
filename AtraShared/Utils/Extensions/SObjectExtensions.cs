c07a58640d770c724660776cd064d1e7c82530e6
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-04-02 22:45:53 -0400

    add new isTrash extension

---
+++ AtraShared/Utils/Extensions/SObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace AtraShared.Utils.Extensions;
+
+internal static class SObjectExtensions
+{
+    /// <summary>
+    /// Gets whether or not an SObject is a trash item.
+    /// </summary>
+    /// <param name="obj">SObject to check.</param>
+    /// <returns>true if it's a trash item, false otherwise.</returns>
+    internal static bool IsTrashItem(this SObject obj)
+        => obj is not null && !obj.bigCraftable.Value && (obj.ParentSheetIndex >= 168 && obj.ParentSheetIndex < 173);
+}

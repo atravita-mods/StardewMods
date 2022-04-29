114501e40c05fbb502e2976db8ceb539f073cc91
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-04-29 19:54:44 -0400

    Initial commit

---
+++ AvoidLosingScepter/AvoidLosingScepter/ModEntry.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using AtraBase.Toolkit.Reflection;
+using AtraShared.ConstantsAndEnums;
+using AtraShared.Utils.Extensions;
+using AtraShared.Utils.HarmonyHelper;
+using HarmonyLib;
+using StardewValley.Tools;
+
+namespace AvoidLosingScepter;
+
+/// <inheritdoc />
+internal class ModEntry : Mod
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    /// <summary>
+    /// Gets the logger for this mod.
+    /// </summary>
+    internal static IMonitor ModMonitor { get; private set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    /// <inheritdoc />
+    public override void Entry(IModHelper helper)
+    {
+        I18n.Init(helper.Translation);
+        ModMonitor = this.Monitor;
+        this.ApplyPatches(new Harmony(this.ModManifest.UniqueID));
+    }
+
+    /// <summary>
+    /// Applies the patches for this mod.
+    /// </summary>
+    /// <param name="harmony">This mod's harmony instance.</param>
+    /// <remarks>Delay until GameLaunched in order to patch other mods....</remarks>
+    private void ApplyPatches(Harmony harmony)
+    {
+        try
+        {
+            HarmonyMethod transpiler = new(typeof(ModEntry), nameof(Transpiler));
+            harmony.Patch(
+                original: typeof(Event).InstanceMethodNamed(nameof(Event.command_minedeath)),
+                transpiler: transpiler);
+            harmony.Patch(
+                original: typeof(Event).InstanceMethodNamed(nameof(Event.command_hospitaldeath)),
+                transpiler: transpiler);
+        }
+        catch (Exception ex)
+        {
+            ModMonitor.Log(string.Format(ErrorMessageConsts.HARMONYCRASH, ex), LogLevel.Error);
+        }
+        harmony.Snitch(this.Monitor, harmony.Id, transpilersOnly: true);
+    }
+
+    private static IEnumerable<CodeInstruction>? Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator gen, MethodBase original)
+    {
+        try
+        {
+            ILHelper helper = new(original, instructions, ModMonitor, gen);
+            helper.FindNext(new CodeInstructionWrapper[]
+            {
+                new(OpCodes.Call, typeof(Game1).StaticPropertyNamed(nameof(Game1.player)).GetGetMethod()),
+                new(OpCodes.Callvirt),
+                new(SpecialCodeInstructionCases.LdLoc),
+                new(OpCodes.Callvirt, typeof(IList<Item>).InstancePropertyNamed("Item").GetGetMethod()),
+                new(OpCodes.Isinst, typeof(MeleeWeapon)),
+                new(OpCodes.Callvirt),
+                new(OpCodes.Ldc_I4_S, 47),
+                new(OpCodes.Beq),
+            });
+
+            int startindex = helper.Pointer;
+
+            helper.FindNext(new CodeInstructionWrapper[]
+            {
+                new(OpCodes.Beq),
+            })
+            .StoreBranchDest()
+            .Push()
+            .AdvanceToStoredLabel()
+            .DefineAndAttachLabel(out Label label)
+            .Pop();
+
+            int endindex = helper.Pointer;
+
+            List<CodeInstruction>? copylist = new();
+            foreach (CodeInstruction? code in helper.Codes.GetRange(startindex, endindex - startindex - 2))
+            {
+                copylist.Add(code.Clone());
+            }
+            copylist[^1].operand = typeof(Wand);
+            copylist.Add(new(OpCodes.Brtrue_S, label));
+            CodeInstruction[]? copy = copylist.ToArray();
+
+            helper.Advance(1)
+            .Insert(copy);
+
+            return helper.Render();
+        }
+        catch (Exception ex)
+        {
+            ModMonitor.Log($"Mod crashed while transpiling mine death methods:\n\n{ex}", LogLevel.Error);
+        }
+        return null;
+    }
+}
---
+++ AvoidLosingScepter/README.md
@@ -0,0 +1,31 @@
+Avoid Losing Specter
+===========================
+
+![Header image](AvoidLosingSpecter/docs/banner.jpg)
+
+This mod prevents you from losing the Return Specter on death.
+
+## Install
+
+1. Install the latest version of [SMAPI](https://smapi.io).
+2. Download this mod and unzip it into `Stardew Valley/Mods`.
+3. Run the game using SMAPI.
+
+## Uninstall
+Simply remove this mod from your Mods folder.
+
+## Configuration
+
+`GiantCropChance` sets the chance a crop will grow big. Note: to ensure that the crop will become giant, probably best to set this slightly above `1`, which is why the default is `1.1`.
+
+## Compatibility
+
+* Works with Stardew Valley 1.5.6 on Linux/macOS/Windows.
+* Works in single player, multiplayer, and split-screen mode. In multiplayer, all players should install.
+* Should be compatible with most other mods. 
+
+* A note: [Moon Misadventures](https://www.nexusmods.com/stardewvalley/mods/10612) has its own way of preventing you from losing certain things on death, so this mod doesn't work if you have that one installed. (I didn't feel like transpiling another mod today.)
+
+## See also
+
+[Changelog](AvoidLosingSpecter/docs/Changelog.md)

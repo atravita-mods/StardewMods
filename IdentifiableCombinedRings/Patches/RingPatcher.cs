2469b94b77dfcaa2da51e33432dd12fc997446f6
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-06-24 00:04:02 -0400

    unfinished project stuff

---
+++ IdentifiableCombinedRings/Patches/RingPatcher.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using StardewValley.Objects;
+
+namespace IdentifiableCombinedRings.HarmonyPatches;
+
+/// <summary>
+/// Class to hold patches on the Ring class.
+/// </summary>
+[HarmonyPatch(typeof(Ring))]
+internal class RingPatcher
+{
+    /// <summary>
+    /// Patches the DisplayName method.
+    /// </summary>
+    /// <param name="__instance">Combined ring to check.</param>
+    /// <param name="__result">Output (the Display Name).</param>
+    [HarmonyPatch(nameof(Ring.DisplayName), MethodType.Getter)]
+    [HarmonyPostfix]
+    [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification ="Harmony convention")]
+    public static void PostfixGetDisplayName(Ring __instance, ref string __result)
+    {
+        if (__instance is CombinedRing combinedRing)
+        {
+            if (combinedRing.combinedRings.Count > 2 || combinedRing.combinedRings[0] is CombinedRing || combinedRing.combinedRings[0] is CombinedRing)
+            {
+                __result += I18n.Many();
+                return;
+            }
+
+            __result = string.Join(" & ", combinedRing.combinedRings.Select((Ring ring) => ring.DisplayName));
+        }
+    }
+}

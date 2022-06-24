92ac1e4dd71d2e273b05529df64e7c78c30e84e7
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-06-23 20:52:29 -0400

    add start of atracore

---
+++ AtraCore/ModEntry.cs
@@ -0,0 +1,39 @@
+﻿using AtraBase.Toolkit.Extensions;
+using AtraBase.Toolkit.Reflection;
+using AtraShared.ConstantsAndEnums;
+using AtraShared.Utils.Extensions;
+using HarmonyLib;
+using StardewModdingAPI.Events;
+using StardewValley.Menus;
+
+namespace AtraCore;
+internal class ModEntry : Mod
+{
+    internal static IMonitor ModMonitor = null!;
+
+    public override void Entry(IModHelper helper)
+    {
+        I18n.Init(helper.Translation);
+        ModMonitor = this.Monitor;
+
+        helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+    }
+
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        this.ApplyPatches(new Harmony(this.ModManifest.UniqueID));
+    }
+
+    private void ApplyPatches(Harmony harmony)
+    {
+        try
+        {
+            harmony.PatchAll();
+        }
+        catch (Exception ex)
+        {
+            ModMonitor.Log(string.Format(ErrorMessageConsts.HARMONYCRASH, ex), LogLevel.Error);
+        }
+        harmony.Snitch(this.Monitor, harmony.Id, transpilersOnly: true);
+    }
+}

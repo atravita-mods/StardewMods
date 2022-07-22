1bcc18a608a2b87b560aaaeba5fbdb20ea131a8e
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-07-22 09:42:31 -0400

    good question.

---
+++ SleepInWedding/ModEntry.cs
@@ -0,0 +1,81 @@
+﻿using AtraShared.ConstantsAndEnums;
+using AtraShared.Integrations;
+using AtraShared.Utils.Extensions;
+using HarmonyLib;
+using StardewModdingAPI.Events;
+
+using AtraUtils = AtraShared.Utils.Utils;
+
+namespace SleepInWedding;
+
+/// <inheritdoc />
+[HarmonyPatch(typeof(GameLocation))]
+internal sealed class ModEntry : Mod
+{
+    /// <summary>
+    /// Gets the config for this mod.
+    /// </summary>
+    internal static ModConfig Config { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets the logging instance for this mod.
+    /// </summary>
+    internal static IMonitor ModMonitor { get; private set; } = null!;
+
+    /// <inheritdoc />
+    public override void Entry(IModHelper helper)
+    {
+        I18n.Init(helper.Translation);
+        ModMonitor = this.Monitor;
+
+        Config = AtraUtils.GetConfigOrDefault<ModConfig>(helper, this.Monitor);
+
+        helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+        helper.Events.GameLoop.SaveLoaded += this.OnSaveLoad;
+
+        this.ApplyPatches(new Harmony(this.ModManifest.UniqueID));
+    }
+
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        GMCMHelper helper = new(this.Monitor, this.Helper.Translation, this.Helper.ModRegistry, this.ModManifest);
+        if (helper.TryGetAPI())
+        {
+            helper.Register(
+                reset: static () => Config = new(),
+                save: () => this.Helper.AsyncWriteConfig(this.Monitor, Config))
+            .GenerateDefaultGMCM(static () => Config);
+        }
+    }
+
+    /// <summary>
+    /// calls queueWeddingsForToday just after save is loaded.
+    /// Game doesn't seem to call it.
+    /// </summary>
+    /// <param name="sender">SMAPI.</param>
+    /// <param name="e">Event args.</param>
+    private void OnSaveLoad(object? sender, SaveLoadedEventArgs e)
+    {
+        if (Context.IsMainPlayer)
+        {
+            Game1.queueWeddingsForToday();
+        }
+    }
+
+    /// <summary>
+    /// Applies the patches for this mod.
+    /// </summary>
+    /// <param name="harmony">This mod's harmony instance.</param>
+    private void ApplyPatches(Harmony harmony)
+    {
+        try
+        {
+            harmony.PatchAll();
+        }
+        catch (Exception ex)
+        {
+            ModMonitor.Log(string.Format(ErrorMessageConsts.HARMONYCRASH, ex), LogLevel.Error);
+        }
+        harmony.Snitch(this.Monitor, this.ModManifest.UniqueID, transpilersOnly: true);
+    }
+}
---
+++ SleepInWedding/WeddingUtils.cs
@@ -0,0 +1,34 @@
+﻿namespace SleepInWedding;
+
+/// <summary>
+/// Utility functions for this mod.
+/// </summary>
+internal static class WeddingUtils
+{
+    /// <summary>
+    /// Checks to see if the farmer is supposed to be getting married today.
+    /// </summary>
+    /// <param name="farmer">farmer.</param>
+    /// <returns>True if they're scheduled for a wedding today, false otherwise.</returns>
+    internal static bool HasWeddingToday(Farmer farmer)
+        => HasWeddingToday(farmer.UniqueMultiplayerID);
+
+    /// <summary>
+    /// Checks to see if the farmer with that specifc multiplayer ID is supposed to be getting married today.
+    /// </summary>
+    /// <param name="multiplayerID">Multiplayer ID of the farmer.</param>
+    /// <returns>True if they're scheduled for a wedding today, false otherwise.</returns>
+    internal static bool HasWeddingToday(long multiplayerID)
+    {
+        if (Game1.weddingsToday.Contains(multiplayerID))
+        {
+            return true;
+        }
+        Farmer farmer = Game1.getFarmerMaybeOffline(multiplayerID);
+        if (farmer is not null && farmer.team.GetSpouse(multiplayerID) is long spouse)
+        {
+            return Game1.weddingsToday.Contains(spouse);
+        }
+        return false;
+    }
+}

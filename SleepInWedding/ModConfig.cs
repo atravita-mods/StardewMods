222af973716dee7cbd46d729b817643c35187fe7
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-07-22 09:42:31 -0400

    good question.

---
+++ SleepInWedding/ModConfig.cs
@@ -0,0 +1,16 @@
+﻿using AtraShared.Integrations.GMCMAttributes;
+
+namespace SleepInWedding;
+
+/// <summary>
+/// The config class for this mod.
+/// </summary>
+internal sealed class ModConfig
+{
+    /// <summary>
+    /// Gets or sets when the wedding should begin.
+    /// </summary>
+    [GMCMInterval(10)]
+    [GMCMRange(600, 2600)]
+    public int WeddingTime { get; set; } = 800;
+}

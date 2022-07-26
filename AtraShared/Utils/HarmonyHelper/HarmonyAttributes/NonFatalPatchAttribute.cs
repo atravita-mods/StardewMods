412e11ceab20c56e2d932ca29d98cb14ccc26b46
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-07-26 19:53:50 -0400

    compat with the extended tools mods.

---
+++ AtraShared/Utils/HarmonyHelper/HarmonyAttributes/NonFatalPatchAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace AtraShared.Utils.HarmonyHelper.HarmonyAttributes;
+
+/// <summary>
+/// Indicates that the patch should be ignored if the target can not be found.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class NonFatalPatchAttribute : Attribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NonFatalPatchAttribute"/> class.
+    /// </summary>
+    public NonFatalPatchAttribute()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NonFatalPatchAttribute"/> class.
+    /// </summary>
+    /// <param name="message">A custom message to add.</param>
+    public NonFatalPatchAttribute(string? message)
+        => this.Message = message;
+
+    /// <summary>
+    /// The custom message to add, if there is one.
+    /// </summary>
+    internal string? Message { get; init; }
+}
---
+++ MuseumRewardsIn/MuseumRewardsIn/AssetManager.cs
@@ -0,0 +1,22 @@
+﻿using AtraBase.Collections;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+
+namespace MuseumRewardsIn;
+
+[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "Preference.")]
+internal static class AssetManager
+{
+    private static readonly string LETTERS_TO_CHECK = PathUtilities.NormalizePath("Mods/atravita/MuseumStore/Letters");
+
+    internal static void Apply(AssetRequestedEventArgs e)
+    {
+        if (e.NameWithoutLocale.IsEquivalentTo(LETTERS_TO_CHECK))
+        {
+            e.LoadFrom(EmptyContainers.GetEmptyDictionary<string, string>, AssetLoadPriority.Low);
+        }
+    }
+
+    internal static HashSet<string> GetMailFlagsForStore()
+        => Game1.temporaryContent.Load<Dictionary<string, string>>(LETTERS_TO_CHECK).Keys.ToHashSet(StringComparer.OrdinalIgnoreCase);
+}

ccc2921801921ee4ec4b6568e6bf7dc39f7930a0
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-10-02 17:19:50 -0400

    New graphics

--- AtraShared/ConstantsAndEnums/ErrorMessageConsts.cs
+++ AtraShared/ConstantsAndEnums/ErrorMessageConsts.cs
@@ -5,5 +5,10 @@
 /// </summary>
 public static class ErrorMessageConsts
 {
+    /// <summary>
+    /// The error message string used for a crash that happens while applying harmony patches.
+    /// </summary>
     public const string HARMONYCRASH = "Mod crashed while applying harmony patches:\n\n{0}";
+
+    public const string STYLECOP_RECORDS = "Records break stylecop :(.";
 }

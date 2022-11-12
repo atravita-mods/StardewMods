a0e4e0b39f3d8fa5d777029a3b3a0b6f84ccef55
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-11-12 12:29:27 -0500

    cleaning

--- LastDayToPlantRedux/Framework/FarmerWatcher.cs
+++ LastDayToPlantRedux/Framework/FarmerWatcher.cs
@@ -8,21 +8,24 @@ namespace LastDayToPlantRedux.Framework;
 /// </summary>
 internal class FarmerWatcher
 {
+    private const int prestiged = Farmer.agriculturist + 100;
+
+    [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Used to listen to an event.")]
     internal void Professions_OnArrayReplaced(NetList<int, NetInt> list, IList<int> before, IList<int> after)
     {
         if ( before.Contains(Farmer.agriculturist) != after.Contains(Farmer.agriculturist)
-                || before.Contains(Farmer.agriculturist + 100) != after.Contains(Farmer.agriculturist + 100))
+                || before.Contains(prestiged) != after.Contains(prestiged))
         {
             CropAndFertilizerManager.RequestReset();
         }
     }
 
+    [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Used to listen to an event.")]
     internal void Professions_OnElementChanged(NetList<int, NetInt> list, int index, int oldValue, int newValue)
     {
-        if (oldValue == Farmer.agriculturist || newValue == Farmer.agriculturist || oldValue == Farmer.agriculturist + 100 || newValue == Farmer.agriculturist + 100)
+        if (oldValue == Farmer.agriculturist || newValue == Farmer.agriculturist || oldValue == prestiged || newValue == prestiged)
         {
             CropAndFertilizerManager.RequestReset();
         }
     }
-
 }

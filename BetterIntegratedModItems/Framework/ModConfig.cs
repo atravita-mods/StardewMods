b12e062b94c70e489a3aba5544263741feb1397c
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-11-22 13:02:15 -0500

    move target framework into common to make updating easier in the future

---
+++ BetterIntegratedModItems/Framework/ModConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace BetterIntegratedModItems.Framework;
+
+/// <summary>
+/// The config class for this mod.
+/// </summary>
+internal sealed class ModConfig
+{
+    public static bool AffectDailyQuests { get; set; } = true;
+}
---
+++ BetterIntegratedModItems/ModEntry.cs
@@ -0,0 +1,54 @@
+﻿using AtraShared.Integrations;
+using AtraShared.MigrationManager;
+using AtraShared.Utils.Extensions;
+
+using BetterIntegratedModItems.Framework;
+
+using StardewModdingAPI.Events;
+
+using AtraUtils = AtraShared.Utils.Utils;
+
+namespace BetterIntegratedModItems;
+
+/// <inheritdoc />
+internal sealed class ModEntry : Mod
+{
+    private MigrationManager? migrator;
+
+    /// <summary>
+    /// Gets the logger for this mod.
+    /// </summary>
+    internal static IMonitor ModMonitor { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets the config instance for this mod.
+    /// </summary>
+    internal static ModConfig Config { get; private set; } = null!;
+
+    /// <inheritdoc />
+    public override void Entry(IModHelper helper)
+    {
+        // bind helpers.
+        ModMonitor = this.Monitor;
+        I18n.Init(helper.Translation);
+
+        // AssetManager.Initialize(helper.GameContent);
+        Config = AtraUtils.GetConfigOrDefault<ModConfig>(helper, this.Monitor);
+
+        helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+    }
+
+    /// <inheritdoc cref="IGameLoopEvents.GameLaunched"/>
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        GMCMHelper helper = new(this.Monitor, this.Helper.Translation, this.Helper.ModRegistry, this.ModManifest);
+        if (helper.TryGetAPI())
+        {
+            helper.Register(
+                reset: static () => Config = new(),
+                save: () => this.Helper.AsyncWriteConfig(this.Monitor, Config))
+            .AddParagraph(I18n.ModDescription)
+            .GenerateDefaultGMCM(static () => Config);
+        }
+    }
+}

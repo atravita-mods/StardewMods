54f69116444b7c5a2b6affd9de94f47827706e80
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-11-26 09:13:29 -0500

    temp stashing this here.

---
+++ AtraCore/Framework/Caches/AssetCache/AssetCacheItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtraCore.Framework.Caches.AssetCache;
+internal class AssetCacheItem<TCache> : IAssetCacheItem<TCache>
+{
+}

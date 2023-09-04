25c2a420134447f7fbf8d0adc98320904da24c8a
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2023-09-04 13:39:31 -0400

    start - move asset cache into AtraCore

--- AtraCore/Framework/Caches/AssetCache/IAssetHolder.cs
+++ AtraCore/Framework/Caches/AssetCache/IAssetHolder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AtraCore.Framework.Caches.AssetCache;
+﻿namespace AtraCore.Framework.Caches.AssetCache;
 
 /// <summary>
 /// A holder for an asset.
@@ -12,7 +6,23 @@ namespace AtraCore.Framework.Caches.AssetCache;
 /// <typeparam name="TOutput">The type of the output.</typeparam>
 public interface IAssetHolder<TOutput>
 {
-    public TOutput Value { get; }
+    /// <summary>
+    /// Gets the value held by this AssetHolder, or null if it's (temporarily) not available.
+    /// </summary>
+    public TOutput? Value { get; }
 
+    /// <summary>
+    /// Gets the asset name associated with this asset.
+    /// </summary>
     public IAssetName AssetName { get; }
+
+    /// <summary>
+    /// Calls the asset again.
+    /// </summary>
+    public void Refresh();
+
+    /// <summary>
+    /// Marks the asset as dirty, to be refreshed the next time it's used.
+    /// </summary>
+    public void MarkDirty();
 }
--- AtraCore/Framework/Caches/AssetCache/ProcessedAssetHolder.cs
+++ AtraCore/Framework/Caches/AssetCache/ProcessedAssetHolder.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace AtraCore.Framework.Caches.AssetCache;
 
-namespace AtraCore.Framework.Caches.AssetCache;
-internal class ProcessedAssetHolder<TAsset, TOutput> : IAssetHolder<TOutput>
+/// <summary>
+/// Holds a reference to an asset that's processed.
+/// </summary>
+/// <typeparam name="TAsset">The type of the asset.</typeparam>
+/// <typeparam name="TOutput">The post-processed type.</typeparam>
+internal sealed class ProcessedAssetHolder<TAsset, TOutput> : IAssetHolder<TOutput>
 {
-    public TOutput Value { get; }
-    public IAssetName AssetName { get; }
+    private readonly IAssetName assetName;
+    private TOutput? cachedOutput;
+    private Func<TAsset, TOutput> del;
+    private bool dirty = false;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProcessedAssetHolder{TAsset, TOutput}"/> class.
+    /// </summary>
+    /// <param name="assetName">The asset name.</param>
+    /// <param name="del">A function to process the output.</param>
+    public ProcessedAssetHolder(IAssetName assetName, Func<TAsset, TOutput> del)
+    {
+        this.assetName = assetName;
+        this.del = del;
+        this.Refresh();
+    }
+
+    /// <inheritdoc />
+    public TOutput? Value
+    {
+        get
+        {
+            if (this.dirty)
+            {
+                this.Refresh();
+            }
+            return this.cachedOutput;
+        }
+    }
+
+    /// <inheritdoc />
+    public IAssetName AssetName => this.assetName;
+
+    /// <inheritdoc />
+    public void MarkDirty() => this.dirty = true;
+
+    /// <inheritdoc />
+    public void Refresh()
+    {
+        var asset = Game1.content.Load<TAsset>(this.assetName.BaseName);
+        this.cachedOutput = this.del(asset);
+        this.dirty = false;
+    }
 }

25480a9166f4b0f1cd666285217b05c504c7221b
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2023-09-05 17:16:56 -0400

    a little birdie told me I'm doing my festival lines wrong

--- AtraCore/Framework/ItemManagement/DataToItemMap.cs
+++ AtraCore/Framework/ItemManagement/DataToItemMap.cs
@@ -156,39 +156,42 @@ public static class DataToItemMap
             {
                 ModEntry.ModMonitor.DebugOnlyLog("Building map to resolve Boots", LogLevel.Info);
 
-                Dictionary<string, int> mapping = new(20);
-                foreach ((int id, string data) in Game1.content.Load<Dictionary<int, string>>(enumToAssetMap[ItemTypeEnum.Boots].BaseName))
+                Dictionary<string, (string id, bool duplicate)> mapping = new(20);
+                foreach ((string id, string data) in Game1.content.Load<Dictionary<string, string>>(enumToAssetMap[ItemTypeEnum.Boots].BaseName))
                 {
                     string name = data.GetNthChunk('/', SObject.objectInfoNameIndex).ToString();
-                    if (!mapping.TryAdd(name, id))
+                    var val = CollectionsMarshal.GetValueRefOrAddDefault(mapping, name, out bool exists);
+                    if (exists)
                     {
-                        ModEntry.ModMonitor.Log($"{name} with {id} seems to be a duplicate Boots and may not be resolved correctly.", LogLevel.Warn);
+                        val.duplicate = true;
+                    }
+                    else
+                    {
+                        val = new(id, false);
                     }
                 }
                 return mapping;
             });
         }
         if (ShouldReset(enumToAssetMap[ItemTypeEnum.BigCraftable])
-            && (!nameToIDMap.TryGetValue(ItemTypeEnum.BigCraftable, out Lazy<Dictionary<string, int>>? bc) || bc.IsValueCreated))
+            && (!nameToIDMap.TryGetValue(ItemTypeEnum.BigCraftable, out var bc) || bc.IsValueCreated))
         {
             nameToIDMap[ItemTypeEnum.BigCraftable] = new(() =>
             {
                 ModEntry.ModMonitor.DebugOnlyLog("Building map to resolve BigCraftables", LogLevel.Info);
 
-                Dictionary<string, int> mapping = new(Game1.bigCraftablesInformation.Count)
+                Dictionary<string, (string id, bool duplicate)> mapping = new(Game1.bigCraftablesInformation.Count)
                 {
                     // special cases
-                    ["House Plant"] = 0,
-                    ["Tub o' Flowers"] = Game1.currentSeason.Equals("winter") || Game1.currentSeason.Equals("fall") ? 109 : 108,
-                    ["Rarecrow 1"] = 110,
-                    ["Rarecrow 2"] = 113,
+                    ["Tub o' Flowers"] = Game1.season is Season.Fall or Season.Winter ? ("109", false) : ("108", false),
+                    ["Rarecrow 1"] = ("110", false),
+                    ["Rarecrow 2"] = ("113", false),
                     ["Rarecrow 3"] = 126,
                     ["Rarecrow 4"] = 136,
                     ["Rarecrow 5"] = 137,
                     ["Rarecrow 6"] = 138,
                     ["Rarecrow 7"] = 139,
                     ["Rarecrow 8"] = 140,
-                    ["Seasonal Plant"] = 184,
                     ["Seasonal Plant 1"] = 188,
                     ["Seasonal Plant 2"] = 192,
                     ["Seasonal Plant 3"] = 196,
@@ -199,9 +202,9 @@ public static class DataToItemMap
                 // House plants :P
                 for (int i = 1; i <= 7; i++)
                 {
-                    mapping["House Plant " + i.ToString()] = i;
+                    mapping["House Plant " + i.ToString()] = (i.ToString(), false);
                 }
-                foreach ((int id, string data) in Game1.bigCraftablesInformation)
+                foreach ((string id, string data) in Game1.bigCraftablesInformation)
                 {
                     ReadOnlySpan<char> nameSpan = data.GetNthChunk('/', SObject.objectInfoNameIndex);
                     if (nameSpan.Equals("House Plant", StringComparison.OrdinalIgnoreCase)

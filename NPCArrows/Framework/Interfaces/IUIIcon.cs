fbdd485c3acc21a1c5c331ec7c2c2cb618070c36
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2023-12-08 11:12:30 -0500

    hook up more of the mail.

---
+++ NPCArrows/Framework/Interfaces/IUIIcon.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+internal interface IUIIcon : IDisposable
+{
+    internal void Draw(SpriteBatch b);
+
+    internal void Reposition(Point p);
+
+    internal void TryHover(int x, int y);
+
+    internal string? GetHoverText();
+
+    internal bool IsDisposed { get; }
+}

f15bf0d29c019ea038da5809b9976cf1ac2e29ac
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2025-01-28 22:27:59 -0600

    add a condition here

--- RenderDisplayRewrite/HarmonyPatches/NopSeasonLoadTranspiler.cs
+++ RenderDisplayRewrite/HarmonyPatches/NopSeasonLoadTranspiler.cs
@@ -22,19 +22,17 @@ internal static class NopSeasonLoadTranspiler
 
     private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        var matcher = new CodeMatcher(instructions);
+        CodeMatcher matcher = new(instructions);
 
         matcher.MatchStartForward(
             new CodeMatch(OpCodes.Ldsfld, AccessTools.Field(typeof(Game1), nameof(Game1.mapDisplayDevice))),
             new CodeMatch((code) => code.IsLdloc()),
-            new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(IDisplayDevice), nameof(IDisplayDevice.LoadTileSheet)))
-        )
+            new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(IDisplayDevice), nameof(IDisplayDevice.LoadTileSheet))))
         .ThrowIfInvalid("mapDisplayDevice")
         .Advance(2)
         .RemoveInstruction()
         .Insert(
-            new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(NopSeasonLoadTranspiler), nameof(AdjustSeasonalTilesheet)))
-            );
+            new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(NopSeasonLoadTranspiler), nameof(AdjustSeasonalTilesheet))));
 
         return matcher.InstructionEnumeration();
     }
@@ -43,8 +41,8 @@ internal static class NopSeasonLoadTranspiler
     {
         if (device is DisplayDevice)
         {
-            // we fake the load so it still throws an error if it's not found.
-            Game1.temporaryContent.Load<Texture2D>(sheet.ImageSource);
+            // we load so it still throws an error if it's not found.
+            Game1.content.Load<Texture2D>(sheet.ImageSource);
         }
         else
         {

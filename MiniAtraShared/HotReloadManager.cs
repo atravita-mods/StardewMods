b26af1691335facebe60e551c098635c95513d14
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2025-07-05 09:46:17 -0500

    dialogue bugfix, thanks Syuchinikurin

---
+++ MiniAtraShared/HotReloadManager.cs
@@ -0,0 +1,24 @@
+﻿#if USING_HARMONY
+using HarmonyLib;
+
+using MiniAtraShared;
+
+[assembly: System.Reflection.Metadata.MetadataUpdateHandler(typeof(HotReloadManager))]
+
+namespace MiniAtraShared;
+
+internal static class HotReloadManager
+{
+    public static void ClearCache(Type[]? types)
+    {
+        var harmony = new Harmony("hot.reload.handler");
+
+        var methods = Harmony.GetAllPatchedMethods().ToArray();
+
+        foreach (var method in methods)
+        {
+            harmony.Patch(original: method);
+        }
+    }
+}
+#endif
